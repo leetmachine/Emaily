09b0e8783319fd45fcc45efe8a69f1887b182c9a
Keegan Gladstone <keegangladstone@gmail.com>
2017-11-24 21:54:04 -1000

    finished auth flow

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
+require('./models/User'); //this must come before ./services/passport because we are trying to use it in passport.
+require('./services/passport'); //nothing is being exported so we do not need to assign a variable.
+
+
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({ bye: "buddy" });
-});
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey]
+    })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+require('./routes/authRoutes')(app); //we are exporting a function from authRoutes, and then immediately call it with app as a parameter.
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
---
+++ services/passport.js
@@ -0,0 +1,38 @@
+//nothing needs to be exported from this file, we only need to make sure the code is run to confgiure the GoogleStrategy in passport.
+//hence we do not need a module.exports or to assign it to a variable.
+
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('../config/keys');
+const mongoose = require('mongoose');
+
+const User = mongoose.model('users');
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+    User.findById(id)
+        .then(user => {
+            done(null, user);
+        });
+});
+
+passport.use(new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback'
+}, (accessToken, refreshToken, profile, done) => {
+    User.findOne({ googleId: profile.id })
+        .then(existingUser => {
+            if (existingUser) {
+                //done takes 2 args err and what to return. In this case we were successful so there is no null.
+                done(null, existingUser);
+            } else {
+                new User({ googleId: profile.id })
+                    .save()
+                    .then(user => done(null, user));
+            }
+        });
+}));
